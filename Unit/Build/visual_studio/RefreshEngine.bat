0c4502211caa302c780cf01df84d62f7e711b47e
Maxwell <maxwell@layer8games.com>
2020-06-29 19:39:28 -0600

    Split out visual tests

--- Unit/Build/visual_studio/RefreshEngine.bat
+++
@@ -1,6 +0,0 @@
-del ..\..\Lib\* /S /Q
-del ..\..\Headers\Engine\ /S /Q
-xcopy ..\..\..\Killer_Engine\Bin\Win32_Debug\Killer_Engine_Win32_Debug.lib ..\..\Lib\ /s /i /y
-xcopy ..\..\..\Killer_Engine\Lib\* ..\..\Lib\ /s /i /y
-xcopy ..\..\..\Killer_Engine\Headers\* ..\..\Headers\ /s /i /y
-PAUSE
