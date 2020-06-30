b6de309c876652dac17fedd04625f3afc4c54394
Maxwell <maxwell@layer8games.com>
2020-06-30 10:01:32 -0600

    Added all files

---
+++ Headers/stdafx.h
@@ -0,0 +1,37 @@
+//===== Prcompiled Headers =====
+//=====Windows Includes=====
+#include <Windows.h>
+
+//=====STD Includes=====
+#include <string>
+#include <memory>
+
+//===== Engine Includes =====
+#include <Engine/Vector2.h>
+#include <Engine/Vector3.h>
+#include <Engine/Vector4.h>
+#include <Engine/Point2.h>
+#include <Engine/Point3.h>
+#include <Engine/Point4.h>
+#include <Engine/Quaternion.h>
+#include <Engine/Matrix3.h>
+#include <Engine/Matrix4.h>
+#include <Engine/Timer.h>
+#include <Engine/Random.h>
+
+#include <Engine/AudioManager.h>
+#include <Engine/ErrorManager.h>
+#include <Engine/EventManager.h>
+#include <Engine/FontManager.h>
+#include <Engine/ShaderManager.h>
+
+#include <Engine/Shader.h>
+#include <Engine/GameWindow.h>
+#include <Engine/Font.h>
+#include <Engine/Keys.h>
+#include <Engine/Input.h>
+
+#include <Engine/AudioClip.h>
+#include <Engine/AudioListener.h>
+
+#include <Engine/Color.h>
